253aa27036c0dacf409514226912a57bbee0ebb3
Matteoscuola24 <mnalvarez.a@iesclarahammerl.com>
2025-02-16 14:07:04 +0100

    inizio conta_soldi
    Una sfida per contare soldi dati e debiti tra due persone

---
+++ conta_soldi.py
@@ -0,0 +1,13 @@
+class Mamma:
+
+    def __init__(self):
+        pass
+
+    def debito_m(self, deb_mamma):
+        self.deb_mamma = float(deb_mamma)
+        piu_m = float(input('quanti soldi devi: '))
+        self.deb_mamma = float(deb_mamma + piu_m)
+
+    def dati_m(self)
+        
+        
