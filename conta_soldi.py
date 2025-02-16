0786850fb4fbc60225efd15b3cb60bc1670d3951
Matteoscuola24 <mnalvarez.a@iesclarahammerl.com>
2025-02-16 14:51:58 +0100

    Update conta_soldi.py

--- conta_soldi.py
+++ conta_soldi.py
@@ -8,6 +8,39 @@ class P1:
         piu_1 = float(input('quanti soldi devi: '))
         self.deb_p1 = float(deb_p1 + piu_1)
 
-    def dati_1(self)
+    def dati_1(self):
+        meno_1 = float(input('quanti soldi hai dato: '))
+        debito_1.deb_p1 = float(deb_p1 - meno_1)
+
+    def mostra(self):
+        return debito_1.deb_p1
+
+class P2:
+
+    def __init__(self):
+        pass
+
+    def debito_2(self, deb_p1):
+        self.deb_p2 = float(deb_p2)
+        piu_2 = float(input('quanti soldi devi: '))
+        self.deb_p2 = float(deb_p2 + piu_2)
+
+    def dati_2(self):
+        meno_2 = float(input('quanti soldi hai dato: '))
+        debito_2.deb_p2 = float(deb_p2 - meno_2)
+
+    def mostra(self):
+        return debito_2.deb_p2
+
+
+# P1.debito(0)
+# P1.dati()
+# P1.mostra()
+
+# P2.debito(0)
+# P2.debito(0)
+# P2.dati()
+# P2.mostra()
+    
         
         
